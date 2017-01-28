584f158001729204823d6db352de5f6ea7dcc728
Kyohei Nakagoe <kyohei.nakagoe@unisys.co.jp>
2017-01-28 22:21:55 +0900

    tour_arrived.html,tour_met.html

--- truffle.js
+++ truffle.js
@@ -1,16 +1,18 @@
 module.exports = {
-  build: {
-    "index.html": "index.html",
-    "app.js": [
-      "javascripts/app.js"
-    ],
-    "app.css": [
-      "stylesheets/app.css"
-    ],
-    "images/": "images/"
-  },
-  rpc: {
-    host: "localhost",
-    port: 8545
-  }
+    build: {
+        "index.html": "index.html",
+        "tour_arrived.html": "tour_arrived.html",
+        "tour_met.html": "tour_met.html",
+        "app.js": [
+            "javascripts/app.js"
+        ],
+        "app.css": [
+            "stylesheets/app.css"
+        ],
+        "images/": "images/"
+    },
+    rpc: {
+        host: "localhost",
+        port: 8545
+    }
 };
