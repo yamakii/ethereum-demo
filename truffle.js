42730a4aa52b4032b6231b82d04718f45efdd672
Kyohei Nakagoe <kyohei.nakagoe@unisys.co.jp>
2017-01-29 11:08:15 +0900

    add qr_display.html,qr_read.html,thankyou.html,tour_finished.html

--- truffle.js
+++ truffle.js
@@ -9,9 +9,13 @@ module.exports = {
         "contracted.html": "contracted.html",
         "tour_arrived.html": "tour_arrived.html",
         "tour_met.html": "tour_met.html",
+        "tour_finished.html": "tour_finished.html",
         "feedback_list.html": "feedback_list.html",
         "feedback_guide.html": "feedback_guide.html",
         "feedback_tourist.html": "feedback_tourist.html",
+        "qr_display.html": "qr_display.html",
+        "qr_read.html": "qr_read.html",
+        "thankyou.html": "thankyou.html",
         "app.js": [
             "javascripts/app.js"
         ],
