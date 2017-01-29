aafbfd96aa73c0c54f4b6a1cf1f5e4c1b22db5ae
NakaoK <nakao4532@gmail.com>
2017-01-29 13:37:40 +0900

    Delete setTimeout() in app.js

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -26,47 +26,31 @@ function deposit(receiver) {
 
   var message =  document.getElementById("comment").value;
   console.log("Send 10 work to deposit wallet");
-  setTimeout(function() {
-    console.log("Initiating transaction... (please wait)");
-  }, 500000000);
+  console.log("Initiating transaction... (please wait)");
 
   console.log("Transaction complete!");
   console.log("Send message to matching partner");
   console.log("Message:");
   console.log(message)
-  setTimeout(() => {
-    console.log("Initiating transaction... (please wait)");
-    tourium.deposit.sendTransaction(reciever, {from: account, value: amount}).then(function(balance) {
-      //var value = tourium.getBalance.call(account).value;
-      //var balance_element = document.getElementById("balance");
-      //balance_element.innerHTML = value;
-      console.log("Transaction complete!");
-    }).catch(function(e) {
-      console.log("Error sending coin; see log.");
-    });
-  }, 500000000);
-
+  console.log("Initiating transaction... (please wait)");
+  tourium.deposit.sendTransaction(reciever, {from: account, value: amount}).then(function(balance) {
+    console.log("Transaction complete!");
+  }).catch(function(e) {
+    console.log("Error sending coin; see log.");
+  });
 };
 
 function payback() {
   var tourium = Tourium.deployed();
   console.log("Receive 10 work to deposit wallet");
-  //setTimeout(() => {
-    console.log("Initiating transaction... (please wait)");
-  //}, 500000000);
-
+  console.log("Initiating transaction... (please wait)");
   console.log("Transaction complete!");
   console.log("Send message to matching partner");
   console.log("Message:");
   console.log(message)
-  //setTimeout(() => {
-    console.log("Initiating transaction... (please wait)");
-  //}, 500000000);
+  console.log("Initiating transaction... (please wait)");
 
   tourium.payback.sendTransaction(reciever, {from: account, value: amount}).then(function() {
-    //value = tourium.getBalance.call(account);
-    //var balance_element = document.getElementById("balance");
-    //balance_element.innerHTML = value;
     console.log("Transaction complete!");
   }).catch(function(e) {
     cosole.log("Error sending coin; see log.");
