3392de59ecc4c2feb66ea8b474c2f7adc37bc519
yamaki <yamaki@wh-plus.com>
2017-01-29 14:58:18 +0900

    会う、着く契約の実装

--- contracts/Tourium.sol
+++ contracts/Tourium.sol
@@ -3,14 +3,29 @@ pragma solidity ^0.4.2;
 contract Tourium {
     address owner;
     address contractor;
+    bool hasMeet;
+    bool hasArrived;
+
+    function Tourium () {
+        hasMeet = false;
+        hasArrived = false;
+    }
 
     function deposit(address _contractor) payable {
         owner = msg.sender;
         contractor = _contractor;
     }
 
+    function arrive() {
+        hasArrived = msg.sender == contractor;
+    }
+
+    function meet() {
+        hasMeet = msg.sender == contractor;
+    }
+
     function payback() returns(bool sufficient) {
-        if (msg.sender != owner) return false;
+        if (msg.sender != owner || !hasArrived || !hasMeet) return false;
         return owner.send(this.balance);
     }
 }
--- test/touium.js
+++ test/touium.js
@@ -13,12 +13,27 @@ contract('Tourium', function(accounts) {
         });
     });
 
+    it("should meet correctly", function() {
+        var sut = Tourium.deployed()
+        return sut.deposit.sendTransaction(accounts[1], {from: accounts[0], value: web3.toWei('1', 'ether')}).then(function() {
+            return sut.arrive({from: accounts[1]});
+        }).then(function() {
+            return sut.meet({from: accounts[1]});
+        }).then(function() {
+            return sut.payback({from: accounts[0]});
+        }).then(function() {
+            return eth.getBalance(sut.address);
+        }).then(function(balance) {
+            assert.equal(web3.fromWei(balance, 'ether') , '0', "Amount wasn't correctly taken from the sender");
+        });
+    });
+
     it("should payback correctly", function() {
         var sut = Tourium.deployed()
         return sut.deposit.sendTransaction(accounts[1], {from: accounts[0], value: web3.toWei('1', 'ether')}).then(function() {
             return eth.getBalance(sut.address);
         }).then(function(balance) {
-            assert.equal(web3.fromWei(balance, 'ether') , '2', "Amount wasn't correctly taken from the sender");
+            assert.equal(web3.fromWei(balance, 'ether') , '1', "Amount wasn't correctly taken from the sender");
             return sut.payback({from: accounts[0]});
         }).then(function() {
             return eth.getBalance(sut.address);
